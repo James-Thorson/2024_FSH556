5f86cbedab5cd99018fa1d30f62e1e159fe84af6
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2024-04-03 08:40:04 -0700

    Week-2 materials

---
+++ Syllabus/Assigning_summaries.R
@@ -0,0 +1,7 @@
+
+students = c("obible", "vhawkin", "annanm", "bmcgreal", "helenm22", "mmin", "smitch88", "krandrup", "nredon", "sulca", "twangs")
+
+set.seed(556)
+
+students = sample(students, replace=FALSE, size=length(students) )
+
